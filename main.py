db09c84ae8f9fd06930dd6a8926d1a7fc13bd911
fernando flores <fernando.floresr@unmsm.edu.pe>
2024-02-07 22:47:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+comments
+"""
+import math
+
+a = math.sqrt(2)  # Square root of 2 is approximately equal to 1.41
+
+print("The square root of 2 is", a)
+
+print("hi there!")
+
+b = math.cos(math.pi / 4)   # The cosine of pi/4 (which is the
+
+c = math.sin(math.pi / 4)    # angle whose tangent is 1, and which is
+
+d = b/c
+
+print(d)
+
+"""
+comments
+"""
+import math
+
+a = math.sqrt(2)  # Square root of 2 is approximately equal to 1.41
+
+print("The square root of 2 is", a)
+
+print("hi there!")
+
+b = math.cos(math.pi / 4)   # The cosine of pi/4 (which is the
+
+c = math.sin(math.pi / 4)    # angle whose tangent is 1, and which is
+
+d = b/c
+
+print(d)
